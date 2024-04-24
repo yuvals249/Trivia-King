0d0fcb8fead091b2990c95657d39391e3ad93693
YUval <yuvals249@gmail.com>
2024-04-24 19:42:02 +0300

    divided def start_game and def check_answers at Server.py into functions to be more readable

--- src/Server.py
+++ src/Server.py
@@ -38,13 +38,8 @@ class Server:
         self.flag = False
         self.socket_server = None  # Initialize socket_server as None
         # get local ip address
-        # for Yuval
         self.host = get_wifi_ip()
-        # for other
-        # self.host = socket.gethostbyname(socket.gethostname())
-
         self.SERVER_TCP_PORT = self.find_available_port()
-
         self.true_statements = {'Y', 'T', 'y', 't', '1'}
         self.false_statements = {'N', 'F', 'n', 'f', '0'}
         print("Server started, listening on IP address %s" % self.host)
@@ -173,163 +168,182 @@ class Server:
         and asking the questions from the questions dictionary until the game ends.
         :return:
         """
-        # keys_list = list(self.barcelona_questions.keys())
         self.game_lost = False
-        round = 1
+        round_number = 1
+
         # Start the game loop
-        while not self.game_lost and round < len(self.questions):
+        while not self.game_lost and round_number < len(self.questions):
+
             # case for the first question to Clients
-            if round == 1:
-                self.currentQuestion = self.askRandomQuestion()
-                # Construct the welcome message
-                welcome_message = (
-                    f"Welcome to the {self.TEAM_NAME} server, where we are answering trivia questions about "
-                    f"any category.\n")
+            if round_number == 1:
+                self.send_welcome_message()
 
-                # Add player names to the welcome message
-                for i, (addr, client_info) in enumerate(self.client_list.items(), start=1):
-                    welcome_message += f"Player {i}: {client_info['team_name']}\n"
-
-                for addr, client_info in self.client_list.items():
-                    try:
-                        client_info['connection'].sendall(welcome_message.encode())
-                        client_info['inGame'] = True
-                    except socket.error:
-                        client_info['inGame'] = False
-                        # self.client_list.pop(addr)
-                # Add the first trivia question to the welcome message
                 time.sleep(5)
-                welcome_message += "==\n"
-                welcome_message = self.currentQuestion
-                # welcome_message += keys_list[0]
-                welcome_message += "question"
 
-                # Send the welcome message to all clients
-                for addr, client_info in self.client_list.items():
-                    try:
-                        client_info['connection'].sendall(welcome_message.encode())
-                    except socket.error:
-                        client_info['inGame'] = False
-                        # self.client_list.pop(addr)
+                self.currentQuestion = self.askRandomQuestion()
+                self.send_question_to_clients()
 
-
-            # add to responses list a tuple that the first value is the team name that answer the current question and
-            # the second value is true if the answer is correct or false either
-            responses = []
-            for addr, client_info in self.client_list.items():
-                try:
-                    if not client_info['inGame']:
-                        continue
-                    client_info['connection'].settimeout(10)
-                    response = client_info['connection'].recv(1024).decode().strip()
-                    # print("Received response from client:", response)
-                    is_correct = (response in self.true_statements and self.questions[self.currentQuestion] == "True"
-                                  or response in self.false_statements and self.questions[self.currentQuestion] == "False")
-                    responses.append((client_info['team_name'], is_correct))
-                except ConnectionResetError:
-                    client_info['inGame'] = False
-                    # self.client_list.pop(addr)
-                except socket.timeout:
-                    print(f"{client_info['team_name']} did not answer in time, so the answer is incorrect")
-                    responses.append((client_info['team_name'], False))
-
+            responses = self.collect_responses()
             self.questions.pop(self.currentQuestion)
+            self.process_responses(responses, round_number)
 
-            # Generate messages for all the Clients based on their responses
-            message = ""
-            for team_name, is_correct in responses:
-                message += f"{team_name} is {'correct!' if is_correct else 'incorrect!'}\n"
-                # Send the message to all players
-            message += "responses"
-            for addr, player_info in self.client_list.items():
-                if player_info['inGame']:
-                    try:
-                        player_info['connection'].sendall(message.encode())
-                    except socket.error:
-                        player_info['inGame'] = False
-                        # self.client_list.pop(addr)
-            time.sleep(6)
-
-            # Check_answers return true if one team stayed and the game ends
-            if self.check_answers(responses, round):
+            if self.game_lost:
                 # End the game
                 end_message = "Game over, sending out offer requests...\n"
                 print(end_message)
+                self.reset_game_state()
 
-                # Close TCP connections
-                for addr, player_info in self.client_list.items():
-                    player_info['connection'].close()
-                self.client_list = {}
-                # Reset the game state
-                self.game_lost = True
+                # Restart broadcasting offers
+                break
             else:
-                round += 1
-        # Restart broadcasting offers
-        self.conn_made = -1
-        self.game_lost = False
+                round_number += 1
 
-    # return True if the game end or False either
-    def check_answers(self, responses, round):
+    def send_welcome_message(self):
         """
-        Check the responses of the players and determine if the game should continue to the next round
-        :param responses: array of tuples containing the team name and a boolean value indicating if the answer is correct
-        :param round: round number
-        :return: true if the game should end, false otherwise
+        Send a welcome message to all clients.
         """
+        welcome_message = f"Welcome to the {self.TEAM_NAME} server, where we are answering trivia questions about " \
+                          f"any category.\n"
+
+        # Add player names to the welcome message
+        for i, (addr, client_info) in enumerate(self.client_list.items(), start=1):
+            welcome_message += f"Player {i}: {client_info['team_name']}\n"
+
+        for addr, client_info in self.client_list.items():
+            try:
+                client_info['connection'].sendall(welcome_message.encode())
+                client_info['inGame'] = True
+            except socket.error:
+                client_info['inGame'] = False
+
+    def send_question_to_clients(self):
+        """
+        Send the trivia question to all clients.
+        """
+        question_message = f"{self.currentQuestion} question"
+        for addr, client_info in self.client_list.items():
+            if client_info['inGame']:
+                try:
+                    client_info['connection'].sendall(question_message.encode())
+                except socket.error:
+                    client_info['inGame'] = False
+
+    def collect_responses(self):
+        """
+        Collect responses from all clients.
+        """
+        responses = []
+        for addr, client_info in self.client_list.items():
+            try:
+                if not client_info['inGame']:
+                    continue
+                client_info['connection'].settimeout(10)
+                response = client_info['connection'].recv(1024).decode().strip()
+                is_correct = (response in self.true_statements and self.questions[self.currentQuestion] == "True"
+                              or response in self.false_statements and self.questions[self.currentQuestion] == "False")
+                responses.append((client_info['team_name'], is_correct))
+            except ConnectionResetError:
+                client_info['inGame'] = False
+            except socket.timeout:
+                responses.append((client_info['team_name'], False))
+                print(f"{client_info['team_name']} did not answer in time, so the answer is incorrect")
+        return responses
+
+    def process_responses(self, responses, round_number):
+        """
+        Process responses from clients and determine game outcome.
+        """
+        self.update_participants_on_results(responses)
+
+        time.sleep(6)
+
         # Get the names of the players who answered correctly
         correct_players = [team_name for team_name, is_correct in responses if is_correct]
         wrong_players = [team_name for team_name, is_correct in responses if not is_correct]
+
         # If all responses are incorrect, continue to next round
         if len(correct_players) == 0:
             messageForNextRound = "No correct answers for this round, all of you continue to the next round"
-            self.generateNextQuestion(messageForNextRound, round)
-            return False
+            self.generateNextQuestion(messageForNextRound, round_number)
+            return
 
         if len(wrong_players) > 0:
-            for team_name in wrong_players:
-                win_message = "Game over!" + "\n"
-                win_message += f"You lost this time: {team_name} !"
-                win_message += "exit"
-                for addr, player_info in self.client_list.items():
-                    if player_info['team_name'] == team_name:
-                        try:
-                            player_info['connection'].sendall(win_message.encode())
-                            player_info['inGame'] = False
-                        except socket.error:
-                            player_info['inGame'] = False
-                            # self.client_list.pop(addr)
+            self.handle_wrong_answers(wrong_players)
 
-        # Only one team answered correct
         if len(correct_players) == 1:
-            # Get the name of the winning player
-            winner_name = correct_players[0]
-            # Send message announcing the winner to all players
-            win_message = "Game over!" + "\n"
-            win_message += f"Congratulations to the winner: {winner_name} "
-            win_message += "exit"
-            for addr, player_info in self.client_list.items():
-                try:
-                    player_info['connection'].sendall(win_message.encode())
-                except socket.error:
-                    player_info['inGame'] = False
-                    # self.client_list.pop(addr)
-
-            # End the game
-            self.game_lost = True
-            return True
+            self.handle_single_winner(correct_players)
 
         # More than one team answered correct
         else:
-            self.generateNextQuestion("", round)
-            return False
+            self.generateNextQuestion("", round_number)
 
-    def generateNextQuestion(self,messageForNextRound, round):
+    def update_participants_on_results(self, responses):
+        # Generate messages for all the Clients based on their responses
+        message = ""
+        for team_name, is_correct in responses:
+            message += f"{team_name} is {'correct!' if is_correct else 'incorrect!'}\n"
+        message += "responses"
+
+        # Send the message to all players
+        for addr, player_info in self.client_list.items():
+            if player_info['inGame']:
+                try:
+                    player_info['connection'].sendall(message.encode())
+                except socket.error:
+                    player_info['inGame'] = False
+
+    def handle_wrong_answers(self, wrong_players):
+        """
+        Handle responses where the answer is incorrect.
+        """
+        for team_name in wrong_players:
+            win_message = "Game over!\n"
+            win_message += f"You lost this time: {team_name} ! exit"
+            for addr, player_info in self.client_list.items():
+                if player_info['team_name'] == team_name:
+                    try:
+                        player_info['connection'].sendall(win_message.encode())
+                        player_info['inGame'] = False
+                    except socket.error:
+                        player_info['inGame'] = False
+
+    def handle_single_winner(self, correct_players):
+        """
+        Handle responses where only one team answered correctly.
+        """
+        # Get the name of the winning player
+        winner_name = correct_players[0]
+        # Send message announcing the winner to all players
+        win_message = "Game over!\n"
+        win_message += f"Congratulations to the winner: {winner_name} exit"
+        for addr, player_info in self.client_list.items():
+            try:
+                player_info['connection'].sendall(win_message.encode())
+            except socket.error:
+                player_info['inGame'] = False
+
+        # End the game
+        self.game_lost = True
+
+    def reset_game_state(self):
+        """
+        Reset the game state.
+        """
+        # Close TCP connections
+        for addr, player_info in self.client_list.items():
+            player_info['connection'].close()
+
+        self.client_list = {}
+        self.conn_made = -1
+        self.game_lost = False
+
+    def generateNextQuestion(self,messageForNextRound, round_number):
         """
         Generate the next trivia question and send it to all players
         :param messageForNextRound: generated message for the next round
         :param round: round number
         """
-        messageForNextRound += f"\nRound {round + 1}, played by"
+        messageForNextRound += f"\nRound {round_number + 1}, played by"
         for addr, client_info in self.client_list.items():
             if client_info['inGame']:
                 messageForNextRound += f" {client_info['team_name']} and"
@@ -337,7 +351,6 @@ class Server:
         messageForNextRound += ":\n"
         messageForNextRound += "==\n"
         # Add the next trivia question to the message
-        # keys_list = list(self.barcelona_questions.keys())
         self.currentQuestion = self.askRandomQuestion()
         messageForNextRound += f"{self.currentQuestion}\n"
         messageForNextRound += "question"
@@ -365,19 +378,14 @@ class Server:
         start the server, start the TCP server and broadcast offers.
         :return:
         """
-        # game_state = "Waiting for clients"
-        # last_client_joined_time = time.time()
 
+        # Start the TCP server thread
         tcp_thread = threading.Thread(target=self.serverTCP)
         tcp_thread.start()
 
+        # Start the UDP server thread
         udp_thread = threading.Thread(target=self.broadcast_offers)
         udp_thread.start()
-        # Start the TCP server thread
-
 
         udp_thread.join()
         tcp_thread.join()
-
-    # def update_players_state(self, names_list):
-    #     for addr, client_info in self.client_list.items:
